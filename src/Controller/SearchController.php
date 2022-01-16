bd39c818d1a0e7d6b38edb35a8e15ee882043cae
Adrian_ <barejaadrian@gmail.com>
2022-01-16 21:08:06 +0100

    Product search

--- src/Controller/SearchController.php
+++ src/Controller/SearchController.php
@@ -5,27 +5,52 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-
+use Symfony\Component\HttpFoundation\Request;
 use App\Repository\ProductRepository;
+use Symfony\Contracts\Translation\TranslatorInterface;
+use Symfony\Component\Serializer\SerializerInterface;
+use App\Repository\AttributeValueRepository;
 
 class SearchController extends AbstractController
 {
-    #[Route('/search/{query}', name: 'search')]
-    public function index(string $query, ProductRepository $productRepository): Response
+    #[Route('/search', name: 'search')]
+    public function index(Request $request, ProductRepository $productRepository, TranslatorInterface $translator, AttributeValueRepository $attributeValueRepository, SerializerInterface $serializer): Response
     {
+        $query = $request->query->get('search');
+        
         $products = $productRepository->search($query);
         $suppliers = [];
+        $attributes = [];
 
         foreach( $products as $product ) {
             if( !in_array($product->getSupplier(), $suppliers) ) {
                 array_push($suppliers, $product->getSupplier());
             } 
+
+            $productAttributes = $product->getProductAttributes();
+
+            foreach( $productAttributes as $productAttribute ) {
+                $attribute = $productAttribute->getAttribute();
+                $attributeValues = $attributeValueRepository->findBy(['attribute' => $attribute]);
+
+                $attributeArr = ['attribute' => $attribute, 'attributeValues' => $attributeValues];
+
+                if( !in_array($attribute, array_column($attributes, 'attribute')) ) {
+                    array_push($attributes, $attributeArr);
+                }
+            }
         }
 
         return $this->render('shop/index.html.twig', [
-            'title' => 'Search',
+            'title' => $translator->trans('Search'),
             'products' => $products,
-            'suppliers' => $suppliers
+            'productsJSON' => $serializer->serialize($products, 'json', [
+                'circular_reference_handler' => function ($object) {
+                    return $object->getId();
+                }
+            ]),
+            'suppliers' => $suppliers,
+            'attributes' => $attributes
         ]);
     }
 }
